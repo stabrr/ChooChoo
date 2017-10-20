8d2dccb54c1c7292f87e7431e52a62119145322b
stabrr <bobbystabile@yahoo.com>
2017-10-19 22:04:22 -0500

    connect to firebase

--- assets/javascript/choochoo.js
+++ assets/javascript/choochoo.js
@@ -1,4 +1,31 @@
+var config = {
+    apiKey: "AIzaSyC4SakjHfRJSc4N2CLCeVAtfiLMC01618I",
+    authDomain: "choochoo-bfcf2.firebaseapp.com",
+    databaseURL: "https://choochoo-bfcf2.firebaseio.com",
+    projectId: "choochoo-bfcf2",
+    storageBucket: "",
+    messagingSenderId: "1074095547745"
+};
+firebase.initializeApp(config);
+var database =firebase.database();
+
 $("#submit-button").on("click", function(){
 	event.preventDefault();
     console.log("i hit submit!");
-});
+    var train=$("#train-name").val().trim();
+    console.log(train);
+    var destination=$("#train-destination").val().trim();
+    console.log(destination);
+    var start=$("#train-start").val().trim();
+    console.log(start);
+    var frequency=$("#train-frequency").val().trim();
+    console.log(frequency);
+
+    var newEmployee = {
+    	name: train,
+    	role: destination,
+    	start: start,
+    	rate: frequency
+    };
+    database.ref().push(newEmployee);
+})
