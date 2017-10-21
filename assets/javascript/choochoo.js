6c40078f19b647f3f16b16307c14881e793243dc
stabrr <bobbystabile@yahoo.com>
2017-10-21 17:29:18 -0500

    working with moment

--- assets/javascript/choochoo.js
+++ assets/javascript/choochoo.js
@@ -42,14 +42,57 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   console.log(childSnapshot.val());
 
   // Store everything into a variable.
-  // var train = childSnapshot.val().name;
-  // var destination = childSnapshot.val().role;
-  // var start = childSnapshot.val().start;
-  // var rate = childSnapshot.val().frequency;
+  var train = childSnapshot.val().name;
+  var destination = childSnapshot.val().role;
+  var start = childSnapshot.val().start;
+  var  frequency= childSnapshot.val().rate;
+  var minutesAway = "10"
 
   // Employee Info
-  // console.log(train);
-  // console.log(destination);
-  // console.log(start);
-  // console.log(rate);
+  console.log(train);
+  console.log(destination);
+  console.log(start);
+  console.log(frequency);
+//
+var tFrequency = frequency;
+
+    // Time is 3:30 AM
+    var firstTime = start;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    console.log("ft"+moment(firstTimeConverted).format("hh:mm"));
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+
+    console.log("ft"+moment(firstTimeConverted).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+    console.log("ARRIVAL TIME: " + nextTrain);
+
+//
+
+
+
+
+
+    $("#employee-table > tbody").append("<tr><td>" + train + "</td><td>" + destination + "</td><td>" +
+  frequency + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td></tr>");
+
  })
